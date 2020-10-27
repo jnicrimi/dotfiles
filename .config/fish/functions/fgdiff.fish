9a319f633f8e14150a18dc383b95f2010702dd18
Junichiro Imai <jnicrimi@gmail.com>
2020-10-28 08:26:55 +0900

    lazy commit : 1 file changed, 22 insertions(+), 8 deletions(-)

--- .config/fish/functions/fgdiff.fish
+++ .config/fish/functions/fgdiff.fish
@@ -1,10 +1,13 @@
 function fgdiff
   set stat 0
+  set range 0
   getopts $argv | while read -l key value
     switch $key
       case _
         printf "invalid option '%s'" $value >&2
         return 1
+      case c
+        set range 1
       case s
         set stat 1
       case \*
@@ -12,14 +15,25 @@ function fgdiff
         return 1
     end
   end
-  set -l target_branch (git branch | fzf | sed -e "s/\* //g" | sed -e 's/^ *//' | awk "{print \$1}")
-  set -l current_branch (git rev-parse --abbrev-ref HEAD)
-  if test -z "$target_branch"
-    return 0
-  end
-  if test $stat -eq 1
-    git diff --stat $target_branch..$current_branch
+  if test $range -eq 1
+    set -l hash (git log --no-merges --pretty=format:"%h - %an : %s" | fzf | cut -d " " -f1)
+    if test -n "$hash"
+      if test $stat -eq 1
+        git diff --stat HEAD..$hash
+      else
+        git diff HEAD..$hash
+      end
+    end
   else
-    git diff $target_branch..$current_branch
+    set -l target_branch (git branch | fzf | sed -e "s/\* //g" | sed -e 's/^ *//' | awk "{print \$1}")
+    set -l current_branch (git rev-parse --abbrev-ref HEAD)
+    if test -z "$target_branch"
+      return 0
+    end
+    if test $stat -eq 1
+      git diff --stat $target_branch..$current_branch
+    else
+      git diff $target_branch..$current_branch
+    end
   end
 end
