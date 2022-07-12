5843a6e8c080d100c9a77e8a09343319cc118955
Junichiro Imai <jnicrimi@gmail.com>
2022-07-12 20:05:57 +0900

    npm

--- setup.sh
+++ setup.sh
@@ -38,6 +38,10 @@ for dot_file in ${dot_files[@]}; do
   create_symlink ${target_file} ${source_file}
 done
 
+source_file="${BASE_DIRECTORY}/package.json"
+target_file="${HOME}/package.json"
+create_symlink ${target_file} ${source_file}
+
 if [ ! -e "${HOME}/.config/git" ]; then
   mkdir "${HOME}/.config/git"
 fi
