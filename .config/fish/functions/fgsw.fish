44a37bd4555e1523c4abb4d25b726c9970b0eb97
Junichiro Imai <jnicrimi@gmail.com>
2024-03-02 15:07:31 +0900

    fix: sed command

--- .config/fish/functions/fgsw.fish
+++ .config/fish/functions/fgsw.fish
@@ -1,5 +1,5 @@
 function fgsw
-  set -l target_branch (git branch | fzf | sed -e "s/\* //g" | sed -e 's/^ *//' | awk "{print \$1}")
+  set -l target_branch (git branch | fzf | sed -e "s/^\* *//g" | awk "{print \$1}")
   if test -n "$target_branch"
     git switch $target_branch
   end
