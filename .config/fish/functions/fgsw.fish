8eec837c8717e1ac1a0fbe73bdbbbeddd57e324b
Junichiro Imai <jnicrimi@gmail.com>
2024-03-02 14:55:25 +0900

    feat: git, checkout to switch

--- .config/fish/functions/fgsw.fish
+++ .config/fish/functions/fgsw.fish
@@ -1,6 +1,6 @@
-function fgco
+function fgsw
   set -l target_branch (git branch | fzf | sed -e "s/\* //g" | sed -e 's/^ *//' | awk "{print \$1}")
   if test -n "$target_branch"
-    git checkout $target_branch
+    git switch $target_branch
   end
 end
