3e179a25d4d997f7a1470d16348b5d3bff02712e
Junichiro Imai <jnicrimi@gmail.com>
2024-08-22 20:42:41 +0900

    chore: remove fish functions

--- .config/fish/functions/fglog.fish
+++
@@ -1,8 +0,0 @@
-function fglog
-  set -l target_branch (git branch | fzf | sed -e "s/\* //g" | sed -e 's/^ *//' | awk "{print \$1}")
-  set -l current_branch (git rev-parse --abbrev-ref HEAD)
-  if test -z "$target_branch"
-    return 0
-  end
-  git log --no-merges $target_branch..$current_branch
-end
