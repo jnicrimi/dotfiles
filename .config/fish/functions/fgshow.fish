b748f0a4d8dfec884d1bf3e5987c9d858c507f75
Junichiro Imai <jnicrimi@gmail.com>
2025-06-15 17:43:25 +0900

    feat: fgshow

--- .config/fish/functions/fgshow.fish
+++ .config/fish/functions/fgshow.fish
@@ -3,9 +3,9 @@ function fgshow
   or return 1
   set -l hash
   if set -lq _flag_path
-    set hash (git log --no-merges --pretty=format:"%h - %an : %s" "$_flag_path" | fzf | cut -d " " -f1)
+    set hash (git log --no-merges --pretty=format:"%H - %an : %s" "$_flag_path" | fzf | cut -d " " -f1)
   else
-    set hash (git log --no-merges --pretty=format:"%h - %an : %s" | fzf | cut -d " " -f1)
+    set hash (git log --no-merges --pretty=format:"%H - %an : %s" | fzf | cut -d " " -f1)
   end
   if test -n "$hash"
     git show $hash
