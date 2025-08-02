474ae74e54fb2c78d73ac9d28649cd57272680ee
Junichiro Imai <jnicrimi@gmail.com>
2025-08-03 03:34:54 +0900

    feat: add interactive file selection with -p option in ggshow

--- .config/fish/functions/ggshow.fish
+++ .config/fish/functions/ggshow.fish
@@ -3,12 +3,17 @@ function ggshow
   _assert_in_git_repository
   or return 1
 
-  argparse -n ggshow 'p/path=' -- $argv
+  argparse -n ggshow 'p/path' -- $argv
   or return 1
 
   set -l commit_hash
   if set -lq _flag_path
-    set commit_hash (_select_commit "$_flag_path")
+    set -l selected_file (git ls-files | fzf --preview 'bat --color=always {} 2>/dev/null || cat {}')
+    if test -z "$selected_file"
+      echo "No file selected" >&2
+      return 0
+    end
+    set commit_hash (_select_commit "$selected_file")
   else
     set commit_hash (_select_commit)
   end
