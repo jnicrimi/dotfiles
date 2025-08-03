6b38adb2ce7f1f3e8ea2e8db79af2275cfe9399c
Junichiro Imai <jnicrimi@gmail.com>
2025-08-03 15:34:22 +0900

    feat: add merge type selection to ggmerge function

--- .config/fish/functions/ggmerge.fish
+++ .config/fish/functions/ggmerge.fish
@@ -7,8 +7,19 @@ function ggmerge
   or return 0
   set -l current_branch (git branch --show-current)
 
-  _confirm_operation "Merge branch" "git merge $selected_branch"
+  set -l merge_type (echo -e "merge\nsquash merge" | fzf --prompt="Merge type: ")
   or return 0
 
-  git merge "$selected_branch"
+  switch $merge_type
+    case "merge"
+      set -l merge_command "git merge $selected_branch"
+      _confirm_operation "Merge branch" "$merge_command"
+      or return 0
+      git merge "$selected_branch"
+    case "squash merge"
+      set -l merge_command "git merge --squash $selected_branch"
+      _confirm_operation "Squash merge branch" "$merge_command"
+      or return 0
+      git merge --squash "$selected_branch"
+  end
 end
