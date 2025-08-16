f1b4ae9f4c226b5d8d91498feeb451474dc35829
Junichiro Imai <jnicrimi@gmail.com>
2025-08-17 00:04:32 +0900

    feat: add fzf-lua plugin

---
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,13 @@
+return {
+  "ibhagwan/fzf-lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("fzf-lua").setup({})
+
+    -- キーマップ設定
+    local map = vim.keymap.set
+    map("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "Find files" })
+    map("n", "<leader>gf", "<cmd>FzfLua git_files<cr>", { desc = "Git files" })
+    map("n", "<leader>gs", "<cmd>FzfLua git_status<cr>", { desc = "Git status" })
+  end
+}
