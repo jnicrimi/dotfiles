f2f80195752a833083e7eed82c1142f4ff1c7ed9
Junichiro Imai <jnicrimi@gmail.com>
2025-08-17 22:53:30 +0900

    feat: add bufferline plugin

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -6,6 +6,7 @@ return {
     local wk = require("which-key")
     wk.setup({})
     wk.add({
+      { "<leader>b", group = "buffer" },
       { "<leader>f", group = "file" },
       { "<leader>g", group = "git" },
     })
