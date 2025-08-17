06b47a2adb212ebf8342a16c5c3cf6958855f303
Junichiro Imai <jnicrimi@gmail.com>
2025-08-18 07:40:29 +0900

    feat: add lsp and completion setup

---
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,53 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      require("mason").setup()
+
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      local has_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+      if has_cmp then
+        capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+      end
+
+      local on_attach = function(_, bufnr)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr })
+      end
+
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "ts_ls",
+          "pyright",
+          "lua_ls",
+          "gopls",
+          "intelephense",
+        },
+        handlers = {
+          function(server_name)
+            require("lspconfig")[server_name].setup({
+              capabilities = capabilities,
+              on_attach = on_attach,
+            })
+          end,
+          ["lua_ls"] = function()
+            require("lspconfig").lua_ls.setup({
+              capabilities = capabilities,
+              on_attach = on_attach,
+              settings = {
+                Lua = {
+                  diagnostics = {
+                    globals = { "vim" },
+                  },
+                },
+              },
+            })
+          end,
+        },
+      })
+    end,
+  },
+}
