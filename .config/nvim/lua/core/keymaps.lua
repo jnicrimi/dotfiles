65ef5730486b9b5344b098e7f3d63e04ea2e6ad9
Junichiro Imai <jnicrimi@gmail.com>
2025-08-23 14:01:20 +0900

    refactor: improve nvim keymaps

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -7,7 +7,12 @@ keymap.set('n', '<Up>', 'gk')
 -- Search
 keymap.set('n', '<Esc><Esc>', ':nohlsearch<CR>', { silent = true })
 
+-- Line movement
+keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move line down' })
+keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move line up' })
+
 -- Buffer operations
+keymap.set('n', '<Tab>', '<cmd>bnext<CR>', { desc = 'Next buffer' })
+keymap.set('n', '<S-Tab>', '<cmd>bprevious<CR>', { desc = 'Previous buffer' })
+keymap.set('n', '<leader><Tab>', '<cmd>b#<CR>', { desc = 'Last buffer' })
 keymap.set('n', '<leader>bd', ':bd<CR>', { desc = 'Delete buffer' })
-keymap.set('n', '<leader>bn', ':bnext<CR>', { desc = 'Next buffer' })
-keymap.set('n', '<leader>bp', ':bprevious<CR>', { desc = 'Previous buffer' })
